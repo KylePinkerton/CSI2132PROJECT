e1436d19cfeee59100293b533c946217c33b24db
Kyle Pinkerton <kpink074@uottawa.ca>
2020-04-07 19:21:34 -0400

    final commit

--- db/generate_table_data.py
+++ db/generate_table_data.py
@@ -1693,7 +1693,7 @@ def reset():
 
   drop table admins, branches, conversation, users, conversation_messages, employees, payment, payment_method,
   payout_method, person, person_email_address, person_phone_number, property, property_taken_dates, property_review, 
-  property_review_details, rental_agreement, user_review, user_review_details, works_at
+  property_review_details, rental_agreement, user_review, user_review_details, works_at CASCADE
 
   """)
   db.commit()
